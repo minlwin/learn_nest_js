689aa8018c7555871f1abdb97cccce9ecc9ef754
minlwin <lwin.zawmin@gmail.com>
2020-02-10 21:56:43 +0630

    Update Service

--- 01.hello-nest-js/01.hello/src/app.service.ts
+++ 01.hello-nest-js/01.hello/src/app.service.ts
@@ -2,7 +2,11 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    return {
+      type: 'Type Script',
+      framework: 'Nest JS',
+      message: 'Hello From Nest JS'
+    }
   }
 }
